d3e664e59f2d2683134cbfe621ffb97c8f0b3d7a
Nathan Mundo <nathanmundo@icloud.com>
2025-12-01 01:09:46 -0600

    style changes to the header

--- Source/main.lua
+++ Source/main.lua
@@ -8,13 +8,32 @@ local font = gfx.font.new('font/Mini Sans 2X')
 gfx.setFont(font)
 
 local function drawHeader(title)
-    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-    gfx.fillRect(0, 0, 400, 20)
-    gfx.drawLine(0, 20, 400, 20)
-    gfx.drawText(title, 16, 2)
+    local titleWidth = gfx.getTextSize(title)
+    local tabWidth = titleWidth + 16
+    local headerHeight = 20
+
+    gfx.fillRect(0, 0, 400, headerHeight)
+
+    -- Draw tab
+    gfx.setColor(gfx.kColorWhite)
+    gfx.fillRect(0, headerHeight / 2, tabWidth, headerHeight / 2)
+    gfx.fillRoundRect(0, 0, tabWidth, headerHeight, 6) -- tab shape
+    gfx.setColor(gfx.kColorBlack)
+
+    -- Very light dotted line at bottom of tab
+    gfx.setColor(gfx.kColorBlack)
+    for x = 0, tabWidth, 4 do
+        gfx.drawPixel(x, headerHeight)
+    end
+    gfx.setColor(gfx.kColorBlack)
+
+    gfx.setImageDrawMode(gfx.kDrawModeFillBlack)
+    gfx.drawText(title, 8, 4)
+
     local time = pd.getTime()
     local timestr = string.format("%02d:%02d", time.hour, time.minute)
     local tw = gfx.getTextSize(timestr)
+    gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
     gfx.drawText(timestr, 400 - tw - 12, 2)
 end
 
